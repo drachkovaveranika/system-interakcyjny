4f5e22d3f77544803d4641bd07a977b101307c1c
Veranika Drachkova <38857885+drachkovaveronika@users.noreply.github.com>
2022-09-14 19:50:13 +0200

    Add files via upload

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,10 +6,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
  /**
   * Class SecurityController.
