11be084a31b77d94bd7b810658cd7d52c0374965
Veranika Drachkova <38857885+drachkovaveronika@users.noreply.github.com>
2022-09-14 08:32:38 +0200

    Add files via upload

---
+++ SecurityController.php
@@ -0,0 +1,49 @@
+<?php
+
+ /*
+  * Security Controller.
+  */
+
+namespace App\Controller;
+
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+ /**
+  * Class SecurityController.
+  */
+class SecurityController extends AbstractController
+{
+    /**
+     * Login.
+     *
+     * @param AuthenticationUtils $authenticationUtils AuthenticationUtils
+     *
+     * @return Response HTTP response
+     */
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * Logout.
+     */
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on firewall.');
+    }
+}
